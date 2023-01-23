6e96edd93c9c1827ce82169082008a4b3d75d700
Julie Gouin <juliegouin@outlook.com>
2023-01-23 17:47:27 +0100

    JS

---
+++ script.js
@@ -0,0 +1,75 @@
+let activePlayer = 1;
+let currentScore = 0;
+let P1globalScore = 0;
+let P2globalScore = 0;
+
+function newGame(){
+    activePlayer = 1;
+    currentScore = 0;
+    P1globalScore = 0;
+    P2globalScore = 0;
+    document.getElementById(`P1globalScore`).textContent = `0`;
+    document.getElementById(`P2globalScore`).textContent = `0`;
+    document.getElementById(`P1currentScore`).textContent = `0`;
+    document.getElementById(`P2currentScore`).textContent = `0`;
+}
+
+function roll() {
+    dice();
+    currentScore = diceNumber + currentScore;
+    let HTMLcurrent = document.getElementById(`P${activePlayer}currentScore`);
+
+    if(diceNumber === 1){
+        currentScore = 0;
+        HTMLcurrent.textContent = '0';
+        switchPlayer();
+    }
+    
+    HTMLcurrent.textContent = `${currentScore}`;
+
+}
+
+function hold(){
+    let HTMLglobal = document.getElementById(`P${activePlayer}globalScore`);
+    let HTMLcurrent = document.getElementById(`P${activePlayer}currentScore`);
+
+    if (activePlayer === 1){
+        P1globalScore = currentScore + P1globalScore;
+        HTMLglobal.textContent =`${P1globalScore}`;   
+    }
+    else {
+        P2globalScore = currentScore + P2globalScore;
+        HTMLglobal.textContent =`${P2globalScore}`;     
+    }
+    
+    currentScore = 0;
+    HTMLcurrent.textContent = `${currentScore}`;
+
+    if(P1globalScore >=100 || P2globalScore >= 100){
+        alert(` Le Player ${activePlayer} a gagné la partie !`);
+    }
+
+    switchPlayer();
+}
+
+function switchPlayer(){
+    if (activePlayer === 1){
+        activePlayer = 2; 
+    }
+    else {
+        activePlayer = 1;
+    }
+}
+
+function dice(){
+
+    diceNumber = getRandomeArbitrairy (1, 7);
+    function getRandomeArbitrairy(min, max){
+        return Math.floor(Math.random() * (max-min) + min);
+    }
+    let getHtmlDice = document.getElementById('Dice');
+    getHtmlDice.innerHTML = `<div class="dice" id="${diceNumber}">
+            <img style="height: 100%" src="Dice/dice-${diceNumber}.png"></img></div>`;
+}
+
+
